40b9428e3931a72a74ea5899afd23a1aea48c702
Girts Silis <girts.silis@gmail.com>
2015-10-23 00:20:54 -0500

    Widget endpoint

--- server/mocks/widget.js
+++ server/mocks/widget.js
@@ -1,35 +1,56 @@
-module.exports = function(app) {
+module.exports = function(app, database) {
   var express = require('express');
   var widgetRouter = express.Router();
 
+  var addUpdateToWidget = function(database, widget) {
+    var update = database.save('update', {
+      created_at: (new Date()).toString(),
+      widget_id: widget.id,
+    });
+
+    widget.update_ids = widget.update_ids || [];
+    widget.update_ids.push(update.id);
+  };
+
   widgetRouter.get('/', function(req, res) {
     res.send({
-      'widget': []
+      widget: database.all('widget'),
     });
   });
 
   widgetRouter.post('/', function(req, res) {
-    res.status(201).end();
+    var rawRecord = JSON.parse(req.requestBody).widget;
+    
+    var record = database.save('widget', rawRecord);
+    addUpdateToWidget(database, record);
+
+    res.send({
+      widget: record,
+    });
   });
 
   widgetRouter.get('/:id', function(req, res) {
     res.send({
-      'widget': {
-        id: req.params.id
-      }
+      widget: database.findById('widget', req.params.id),
     });
   });
 
   widgetRouter.put('/:id', function(req, res) {
+    var rawRecord = JSON.parse(req.requestBody).widget;
+    var record = database.save('widget', rawRecord);
+    addUpdateToWidget(database, record);
+  
     res.send({
-      'widget': {
-        id: req.params.id
-      }
+      widget: record,
     });
   });
 
   widgetRouter.delete('/:id', function(req, res) {
-    res.status(204).end();
+    if (database.destroy('widget', req.params.id)) {
+      res.status(204).end();
+    } else {
+      res.status(404).end();
+    }
   });
 
   app.use('/api/widget', widgetRouter);
