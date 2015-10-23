cdb3fe86864dc45b22f3e49d734529e5a35b70dd
Girts Silis <girts.silis@gmail.com>
2015-10-23 00:26:56 -0500

    Update resource endpoint

---
+++ server/mocks/update.js
@@ -0,0 +1,20 @@
+module.exports = function(app, database) {
+  var express = require('express');
+  var updateRouter = express.Router();
+
+  updateRouter.get('/:id', function(req, res) {
+    res.send({
+      update: database.findById('update', req.params.id),
+    });
+  });
+
+  updateRouter.delete('/:id', function(req, res) {
+    if (database.destroy('update', req.params.id)) {
+      res.status(204).end();
+    } else {
+      res.status(404).end();
+    }
+  });
+
+  app.use('/api/update', updateRouter);
+};
